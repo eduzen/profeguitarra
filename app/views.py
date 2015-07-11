d1dcc59d8e6c02b345ec996b1fb719567f5e2199
eduzen <eduardo.a.enriquez@gmail.com>
2015-07-11 20:47:48 -0300

    se editaron las views y se creo la page cursoguitarra

--- app/views.py
+++ app/views.py
@@ -11,6 +11,42 @@ def index():
     return render_template('body.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/sobremi')
+def sobremi():
+    return render_template('sobremi.html')
+
+
+@app.route('/clasesdeguitarra')
+def guitarraelectrica():
+    return render_template('clasesdeguitarra.html')
+
+
+@app.route('/guitarraclasicaocriolla')
+def guitarraclasicaocriolla():
+    return render_template('guitarraclasicaocriolla.html')
+
+
+@app.route('/guitarraacustica')
+def guitarraacustica():
+    return render_template('guitarraacustica.html')
+
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+
+@app.route('/cursodeguitarra')
+def cursodeguitarra():
+    return render_template('cursodeguitarra.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login')
+
+
+@app.route('/formacionmusical')
+def formacion():
+    return render_template('formacion')
+
